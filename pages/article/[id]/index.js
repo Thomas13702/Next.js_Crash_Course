6f53489d7d238bd62dcf179759b370f662ea8497
Thomas13702 <thomaspritchard137@btinternet.com>
2021-05-03 15:02:10 +0100

    getStaticPaths()

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -18,7 +18,24 @@ const article = ({ article }) => {
 //Data fetching method next js provides to pages
 // get server side props will fetch data at time of request
 
-export const getServerSideProps = async (context) => {
+// export const getServerSideProps = async (context) => {
+//   //context can be used to get ID from URL
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   ); //get individual post
+
+//   const article = await res.json();
+
+//   return {
+//     props: {
+//       article,
+//     },
+//   };
+// };
+
+// Dynamically generating paths to data
+//fetched at build time
+export const getStaticProps = async (context) => {
   //context can be used to get ID from URL
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
@@ -30,6 +47,24 @@ export const getServerSideProps = async (context) => {
     props: {
       article,
     },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  //get all artciles
+
+  const articles = await res.json();
+
+  const ids = articles.map((article) => article.id);
+  //creating an array of id for each article
+
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+  //maps through ids and returns then in a format like params: { id: "1", id: "2" } does this for every article
+
+  return {
+    paths,
+    fallback: false, //if we go to data that doesnt exist will return a 404
   };
 };
 
